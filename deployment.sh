46a4c9096aced6b124d32ecdfb76303f459b0027
koobe <koobe.liu@uaf.com.cn>
2022-12-12 09:39:37 +0800

    添加dockerfile文件

---
+++ deployment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cargo build -r
+
+#停止服务
+docker stop rust-admin
+
+
+#删除容器
+docker rm rust-admin
+
+#删除镜像
+docker rmi rust-admin:v1
+
+#删除none镜像
+docker rmi $(docker images | grep "none" | awk '{print $3}')
+
+#构建服务
+docker build -t rust-admin:v1 -f Dockerfile .
+
+#启动服务
+docker run -itd --net=host --name=rust-admin rust-admin:v1
